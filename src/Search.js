507f39c158db613c2c91c991c4f32235ee97ceaf
denisvfedas <denisvfedas@gmail.com>
2017-09-17 12:14:02 -0400

    Search.js and Search.css created

---
+++ src/Search.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import {browserHistory as history} from 'react-router';
+
+class Search extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      city: ""
+    };
+  }
+
+  _submitHandle = (event) => {
+    event.preventDefault();
+    history.push(`/${this.refs.cityInput.value}`);
+    this.setState({
+      city: this.refs.cityInput.value
+    });
+  }
+
+  render() {
+    console.log("this.state.city", this.state.city);
+    return(
+      <div className="Search">
+        <form onSubmit={this._submitHandle}>
+          <input type="text" ref="cityInput" className="Search-form-input"/>
+          <button className="Search-form-button"><i className="fa fa-search"/></button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Search;
