704f5f051319d1a3ecdf5908eaa6707f2304d3b5
denisvfedas <denisvfedas@gmail.com>
2017-09-17 12:08:09 -0400

    Search.js and Search.css added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
+import {Link} from 'react-router';
 import './App.css';
+import Search from './Search';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      "city": ""
+    };
+  }
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-
-        </div>
-        
+        <header className="App-header">
+          <Link to="/">WeatherApp</Link>
+          <Search />
+        </header>
+        <main className="App-main">
+          {this.props.children}
+        </main>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const routes = (
+  <Router history={browserHistory}>
+    <Route path="/" component={App}>
+
+    </Route>
+  </Router>
+);
+
+ReactDOM.render(routes, document.getElementById('root'));
 registerServiceWorker();
