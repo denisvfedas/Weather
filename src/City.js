ba3ae814423cddc83b56b0fd933086321a18ce90
denisvfedas <denisvfedas@gmail.com>
2017-10-03 21:06:09 -0400

    icons added, UnitBtn created

--- src/City.js
+++ src/City.js
@@ -1,25 +1,28 @@
 import React from 'react';
+import UnitBtn from './UnitBtn';
 import './City.css';
 
 class City extends React.Component {
   constructor() {
     super();
     this.state = {
-      unit: "ca"
-      // city: "Montreal",
-      // location: {
-      //   lat: 45.5016889,
-      //   lng: -73.567256
-      // },
-      // forecast: {
-      //   currently: {
-      //     temperature: 87.27
-      //   }
+      unit: true
     }
   }
 
+  _handleClick = () => {
+    this.setState({
+      unit: !this.state.unit
+    })
+  }
+
   componentDidMount = () => {
-    let unit = this.state.unit;
+    let unit;
+    if (this.state.unit) {
+      unit = "ca" ;
+    } else {
+      unit = "us";
+    };
     let name = this.props.params.city;
     let cityUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${name}&key=AIzaSyCbsNCa4SMcWhL10v9oYWNHnXC1dRWNnT4`;
     //Proxy server https://cors-anywhere.herokuapp.com/ to prevent CORS problems
@@ -43,8 +46,13 @@ class City extends React.Component {
   }
 
   componentDidUpdate = () => {
-    if(this.state.city !== this.props.params.city) {
-      let unit = this.state.unit;
+    let unit;
+    if (this.state.unit) {
+      unit = "ca" ;
+    } else {
+      unit = "us";
+    };
+    if(this.state.city !== this.props.params.city || unit !== this.state.forecast.flags.units) {
       let name = this.props.params.city;
       let cityUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${name}&key=AIzaSyCbsNCa4SMcWhL10v9oYWNHnXC1dRWNnT4`;
       let forecastUrl = "https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/0046ffbcf26d5b029c9499baa0c950b9/";//[lat],[lng]
@@ -78,13 +86,21 @@ class City extends React.Component {
       return(
         <div className="City-loaded">
           <div className="City-loaded-city">
-            <h2>{this.state.city}</h2>
-            <p>{this.state.forecast.hourly.summary}</p>
+            <h1>{this.state.city}</h1>
+            <UnitBtn handleClick={this._handleClick} unit={this.state.unit}/>
           </div>
+          <p>{this.state.forecast.hourly.summary}</p>
           <div className="City-loaded-temp">
-            <img src="clear-night.jpg" alt="clear-night" height="62" width="62"/>
-            <h2>{this.state.forecast.currently.temperature}</h2>
-            <p>Feels like {this.state.forecast.currently.apparentTemperature}</p>
+            <img src={require(`./icons/${this.state.forecast.currently.icon}.png`)} alt={`${this.state.forecast.currently.icon}`} />
+            <h1>
+              {Math.round(this.state.forecast.currently.temperature)}
+            </h1>
+            <div className="City-loaded-temp-cont">
+              <h2>{this.state.unit ? "°C" : "°F"}</h2>
+              <p>Feels like<br/>
+                {Math.round(this.state.forecast.currently.apparentTemperature)}
+              </p>
+            </div>
           </div>
           <h2>{this.state.forecast.currently.summary}</h2>
         </div>
